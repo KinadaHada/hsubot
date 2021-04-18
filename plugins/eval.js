a33ae1bcf4bac19850b82a07ea071b436c164bfb
KinadaHada <76767551+KinadaHada@users.noreply.github.com>
2021-04-18 18:34:13 +0800

    Create eval.js

---
+++ plugins/eval.js
@@ -0,0 +1,20 @@
+let eval = {
+    name: 'evals',
+    status: true,
+    clue: ['Fungsi: evaluator', 'Format:\n <code>.eval [code untuk dieksekusi]</code>'],
+    regex: /^([!\/\.]evals)$/i,
+    run: async function (tg, update) {
+        let message = update.message
+        let text = message.content.text.text
+
+        if (this.regex.exec(text)) {
+          var evals = this.regex.exec(text)
+          var evals2 = text.replace(evals[1], "")
+          try {
+			await eval(`(async ()=>{ ${evals2} })()`);
+		} catch (err) {
+			tg.sendMessage(message.chat_id, String(err));
+		}
+        }
+    }
+}
